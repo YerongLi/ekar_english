1c7cfd49008cbe4484464cb67d1bdd5f433ceb9a
Yerong Li <yerong.li@outlook.com>
2023-03-16 18:46:53 -0500

    Use GPU

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=""
+export CUDA_VISIBLE_DEVICES="4,5,6,7"
 python run_swag.py \
 --model_name_or_path roberta-base \
 --do_train \
@@ -6,6 +6,6 @@ python run_swag.py \
 --learning_rate 5e-5 \
 --num_train_epochs 3 \
 --output_dir /tmp/swag_base \
---per_gpu_eval_batch_size=1 \
---per_device_train_batch_size=1 \
+--per_gpu_eval_batch_size=4 \
+--per_device_train_batch_size=4 \
 --overwrite_output
