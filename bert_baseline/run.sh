73fce2b87449b30665f0bc5d4c58f67ed8c9534d
Yerong Li <yerong.li@outlook.com>
2023-03-16 17:34:20 -0500

    Add run.sh script

---
+++ bert_baseline/run.sh
@@ -0,0 +1,11 @@
+export CUDA_VISIBLE_DEVICES=0,1
+python run_swag.py \
+--model_name_or_path roberta-base \
+--do_train \
+--do_eval \
+--learning_rate 5e-5 \
+--num_train_epochs 3 \
+--output_dir /tmp/swag_base \
+--per_gpu_eval_batch_size=1 \
+--per_device_train_batch_size=1 \
+--overwrite_output
