7ca42a9a52d89e44a2a7f6ec05fb62684d3654c6
Yerong Li <yerong.li@outlook.com>
2023-03-20 00:41:17 -0500

    Break after the first iteration

--- DEER-main/analysis.py
+++ DEER-main/analysis.py
@@ -2,5 +2,5 @@ import json
 dataset = json.load(open('extract_wiki/dataset_0.50_undir2dir_0.74.json'))
 for i, datapoint in enumerate(dataset):
 	# dict_keys(['pair', 'sim', 'entity', 'target', 'source', 'triple'])
-	print(datapoint['pairs']])
+	print(datapoint['pairs'])
 	if i > 5: break
