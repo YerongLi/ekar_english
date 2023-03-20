8ded39647d6012211a051deaa5a3adff3c880a91
Yerong Li <yerong.li@outlook.com>
2023-03-19 23:23:53 -0500

    test install_mp_handler

--- DEER-main/test.py
+++ DEER-main/test.py
@@ -1,12 +1,14 @@
 import logging
 import multiprocessing
 import tqdm
+from multiprocessing_logging import install_mp_handler
+
 logging.basicConfig(filename='test.log', 
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         level=logging.DEBUG)
-
-logger =  multiprocessing.get_logger()
+install_mp_handler()
+logger =  multiprocessing.get_logger(__name_)
 logger.info('start')
 def p(i):
 	logger.info(i)
