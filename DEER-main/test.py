ae84bf60ea42df397bfa80d8effa363a08261e39
Yerong Li <yerong.li@outlook.com>
2023-03-19 23:13:26 -0500

    Log to test.log file

--- DEER-main/test.py
+++ DEER-main/test.py
@@ -1,6 +1,11 @@
 import logging
 import multiprocessing
 logging(filename='test.log')
+logging.basicConfig(filename='test.log', 
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.DEBUG)
+
 logger =  multiprocessing.get_logger()
 
 def print(i):
