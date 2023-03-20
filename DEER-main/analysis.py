dc8f2cf06f78a94a090c5c1e74200fe11c9f7407
Yerong Li <yerong.li@outlook.com>
2023-03-20 00:42:07 -0500

    Break after the first iteration

--- DEER-main/analysis.py
+++ DEER-main/analysis.py
@@ -1,6 +1,6 @@
 import json
 import logging
-
+import os
 logging.basicConfig(filename='output.log', 
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
