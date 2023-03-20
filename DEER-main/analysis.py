0ecf739e07c16ab8e6bfe8d1253f481aa6e7f536
Yerong Li <yerong.li@outlook.com>
2023-03-20 00:41:58 -0500

    Break after the first iteration

--- DEER-main/analysis.py
+++ DEER-main/analysis.py
@@ -1,6 +1,16 @@
 import json
+import logging
+
+logging.basicConfig(filename='output.log', 
+        format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
+        datefmt='%Y-%m-%d %H:%M:%S',
+        level=logging.DEBUG)
+# install_mp_handler()
+logger = logging.getLogger(__name__)
+logger.info(f'Logger start: {os.uname()[1]}')
+
 dataset = json.load(open('extract_wiki/dataset_0.50_undir2dir_0.74.json'))
 for i, datapoint in enumerate(dataset):
 	# dict_keys(['pair', 'sim', 'entity', 'target', 'source', 'triple'])
-	print(datapoint['pairs'])
+	logger.info(datapoint['pairs'])
 	if i > 5: break
