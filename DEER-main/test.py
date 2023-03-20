19e83b36ca5acb2b1cc91f0e957a6fb10f201f02
Yerong Li <yerong.li@outlook.com>
2023-03-19 23:14:45 -0500

    Remove wrong logging configuration

--- DEER-main/test.py
+++ DEER-main/test.py
@@ -1,6 +1,6 @@
 import logging
 import multiprocessing
-
+import tqdm
 logging.basicConfig(filename='test.log', 
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
