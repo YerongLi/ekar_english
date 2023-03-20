6c539b5f70b0a854800fd140cee21d5d60410deb
Yerong Li <yerong.li@outlook.com>
2023-03-20 00:43:56 -0500

    debug : use key 'pair' instead of 'pairs'

--- DEER-main/analysis.py
+++ DEER-main/analysis.py
@@ -12,5 +12,5 @@ logger.info(f'Logger start: {os.uname()[1]}')
 dataset = json.load(open('extract_wiki/dataset_0.50_undir2dir_0.74.json'))
 for i, datapoint in enumerate(dataset):
 	# dict_keys(['pair', 'sim', 'entity', 'target', 'source', 'triple'])
-	logger.info(datapoint['pairs'])
+	logger.info(datapoint['pair'])
 	if i > 5: break
