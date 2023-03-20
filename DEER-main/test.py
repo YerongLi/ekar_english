4c2fb62cf6f3033996b750be0041ebd54ff29f8c
Yerong Li <yerong.li@outlook.com>
2023-03-19 23:14:06 -0500

    Remove wrong logging configuration

--- DEER-main/test.py
+++ DEER-main/test.py
@@ -1,6 +1,6 @@
 import logging
 import multiprocessing
-logging(filename='test.log')
+
 logging.basicConfig(filename='test.log', 
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
