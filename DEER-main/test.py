df267ac908fb0bc13a6652f60278732b9830b2fc
Yerong Li <yerong.li@outlook.com>
2023-03-19 23:21:39 -0500

    test get_logger(__name__)

--- DEER-main/test.py
+++ DEER-main/test.py
@@ -1,20 +1,16 @@
 import logging
 import multiprocessing
 import tqdm
-
-from multiprocessing_logging import install_mp_handler
 logging.basicConfig(filename='test.log', 
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         level=logging.DEBUG)
 
-install_mp_handler()
-logger = logging.getLogger(__name__)
-
+logger =  multiprocessing.get_logger()
 logger.info('start')
-def pt(i):
+def p(i):
 	logger.info(i)
 with multiprocessing.Pool(2) as p:
-  _ = tqdm.tqdm(p.imap(pt, range(30)), total=30)
+  _ = tqdm.tqdm(p.imap(p, range(30)), total=30)
 
 
