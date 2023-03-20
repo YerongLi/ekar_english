57b3605bab5835901358900d6a530da78f9a62c7
Yerong Li <yerong.li@outlook.com>
2023-03-20 00:54:39 -0500

    log the key as well

--- DEER-main/analysis.py
+++ DEER-main/analysis.py
@@ -13,5 +13,6 @@ dataset = json.load(open('extract_wiki/dataset_0.50_undir2dir_0.74.json'))
 for i, datapoint in enumerate(dataset):
 	# dict_keys(['pair', 'sim', 'entity', 'target', 'source', 'triple'])
 	k = 'triple'
+	logger.info(k)
 	logger.info(datapoint[k])
 	if i > 5: break
