0f97a6840a1ce1c841993cc26095b7029130508b
Yerong Li <yerong.li@outlook.com>
2023-03-23 15:32:57 -0500

    add uft8 for logger

--- DEER-main/everydayentity.py
+++ DEER-main/everydayentity.py
@@ -8,6 +8,7 @@ logging.basicConfig(filename='output.log',
         format='%(asctime)s.%(msecs)03d %(levelname)s %(module)s - %(funcName)s: %(message)s',
         datefmt='%Y-%m-%d %H:%M:%S',
         level=logging.DEBUG)
+logging.FileHandler(log_file, encoding='utf8')
 install_mp_handler()
 logger = logging.getLogger(__name__)
 logger.info(f'Logger start: {os.uname()[1]}')
